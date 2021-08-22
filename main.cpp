29b2910d540ebe13275f8d19f163420947feb188
Olive <Olive.bien.noir@gmail.com>
2021-08-22 14:42:04 +0200

    Restauration de l'ancienne version de Merge sort

--- main.cpp
+++ main.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
-#include <chrono>
-#include <ctime>
 
 #include "./Sort.hpp"
 
-template<typename Lambda, std::intmax_t num = 1, std::intmax_t den = 1000>
-double MesureInstructionsTime(Lambda instructions) {
-
-    auto start = std::chrono::high_resolution_clock::now();
-    instructions();
-
-    return std::chrono::duration<double, std::ratio<num, den>>(std::chrono::high_resolution_clock::now()-start).count();
-
-}
-
-void BubbleSort(std::vector<int> &v) { Sort::Bubble(v, Comparison::croissant<int>); }
-void MergeSort(std::vector<int> &v) { Sort::Merge(v, Comparison::croissant<int>); }
-
 int main() {
 
-    std::srand(std::time(0));
+    std::vector<int> numbers{1, 6, 34, -56, -7, 23};
 
-    double averageBubble{0}, averageMerge{0};
+    std::cout << "Voici le vecteur : \n";
+    for (int i : numbers) {
 
-    for (std::size_t precision{0}; precision < 2000; precision++) {
-
-        std::vector<int> numbers;
-        for (std::size_t j{0}; j < precision; j++) {
-
-            numbers.push_back((-RAND_MAX/2)+std::rand()%RAND_MAX);
-
-        }
-
-        averageMerge += MesureInstructionsTime([&numbers]() -> void { MergeSort(numbers); });
-        averageBubble += MesureInstructionsTime([&numbers]() -> void { BubbleSort(numbers); });
+        std::cout << i << ' ';
 
     }
 
-    std::cout << "Temps moyen pour MergeSort : " << averageMerge/1000 << " ms\nTemps moyen pour BubbleSort : " << averageBubble/1000 << " ms";
+    Sort::Merge(numbers, Comparison::croissant<int>);
+
+    std::cout << "\n\nVoici le vecteur trie dans l'ordre croissant : \n";
+    for (int i : numbers) {
+
+        std::cout << i << ' ';
+
+    }
+
     std::cin.get();
 
     return 0;
